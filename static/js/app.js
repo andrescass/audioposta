541ba92f9fa3c69c4ff2970306eb143c4f068702
andrescass <andres.cass@gmail.com>
2021-05-12 01:48:11 -0300

    added podcast get api

--- static/js/app.js
+++ static/js/app.js
@@ -21,7 +21,7 @@ stopButton.addEventListener("click", stopRecording);
 
 // Auxiliar flags
 var hasRecorded = false;
-var baseServerUrl = "https://postarecorder.pythonanywhere.com"
+var baseServerUrl = "http://postarecorder.pythonanywhere.com"
 //var baseServerUrl = "http://127.0.0.1:8000"
 
 // Timers
